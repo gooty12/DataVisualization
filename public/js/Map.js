877c14600b75a569178d9169fd4a646bab661a43
Krunal Jain <krunal.bitsy@gmail.com>
2018-11-09 22:02:50 -0700

    Commit changes

---
+++ public/js/Map.js
@@ -0,0 +1,87 @@
+
+
+class CountryData {
+
+    constructor(id, countryName, countryId, year, gold, silver, bronze, properties, geometry) {
+
+       this.id = id;
+       this.countryName = countryName;
+       this.year = year;
+       this.gold = gold;
+       this.silver = silver;
+       this.countryId = countryId;
+       this.bronze = bronze;
+       this.properties = properties;
+       this.geometry = geometry;
+    }
+}
+
+
+class WorldMap{
+
+    constructor(yearAggregate, reverseMappings) {
+        this.projection = d3.geoWinkel3().scale(140).translate([365, 225]);
+
+        this.reverseMappings = reverseMappings;
+        this.yearAggregate = yearAggregate;
+
+
+
+    }
+
+
+    drawMap(world, year) {
+
+
+        let geojson = topojson.feature(world, world.objects.countries);
+        // console.log(geojson.features);
+        // console.log(this.populationData);
+        //console.log(this.nameArray);
+        let currentYearData = this.yearAggregate[year];
+        console.log(geojson.features)
+
+        let countryData = geojson.features.map(country => {
+            let countryID = this.reverseMappings[country.id.toLowerCase()];
+
+            if(countryID && currentYearData[countryID]) {
+
+                let gold = currentYearData[countryID.toUpperCase()]['medals']['gold']
+                let silver = currentYearData[countryID.toUpperCase()]['medals']['silver']
+                let bronze = currentYearData[countryID.toUpperCase()]['medals']['bronze']
+                return new CountryData(country.id, "", this.reverseMappings[country.id], year, gold, silver, bronze, country.properties, country.geometry)
+            }
+            else {
+                return new CountryData(country.id, "", "NA", year, 0, 0, 0, country.properties, country.geometry)
+
+            }
+
+        });
+
+        console.log(countryData)
+        let path = d3.geoPath().projection(this.projection);
+        let graticule = d3.geoGraticule();
+        let svg = d3.select("#map-chart").append("svg");
+
+        svg.attr("class", "countries boundary")
+            .selectAll("path")
+            .data(countryData)
+            .enter().append("path")
+            .attr("d", path)
+            .attr("id", d => d.id)
+            .attr("class", d => d.region)
+
+        svg.append("path")
+            .datum({type: "Sphere"})
+            .attr("id", "sphere")
+            .attr("d", path)
+            .classed("stroke", true)
+
+
+
+        svg.append("path")
+            .datum(graticule)
+            .classed("graticule", true)
+            .attr("d", path);
+    }
+
+}
--- public/js/script.js
+++ public/js/script.js
@@ -1,155 +1,239 @@
+loadData().then(data => {
 
-function init() {
-    initMap();
-}
+    this.activeCountry = null;
+    this.activeYear = '2012';
+    let self = this;
+    let olympicsData = data['olympicsData'];
+    let countryData = data['countryData'];
+    setMapIds(countryData, olympicsData)
+    console.log(olympicsData)
 
 
-function initMap() {
-    let olympicsData = {}
-    let countryData = {}
 
-    d3.csv("data/dictionary.csv").then(countries => {
-        let geocoder = new google.maps.Geocoder;
-        countryData = countries
-        console.log(countryData)
 
-        d3.csv("data/pop.csv").then(data => {
-            //console.log(data)
-            let discrepentList = [];
 
-            for(let i = 0; i < countryData.length; i++) {
-                let discrepent = true;
-                for(let j = 0; j < data.length; j++) {
-                    if((countryData[i]['Country'].toLowerCase() === data[j]['country'].toLowerCase()) || (countryData[i]['Code'].toLowerCase() === data[j]['geo'].toLowerCase())) {
-                        discrepent = false;
-                        countryData[i]['mapId'] = data[j]['geo'];
-                        break;
-                    }
-                }
-                if(discrepent) {
-                    countryData[i]['mapId'] = 'NA';
-                    discrepentList.push(countryData[i])
-                }
+    //// Creates the view objects
+    //const worldMap = new Map(data, updateCountry);
+
+    d3.json('data/world.json').then(mapData => {
+        let yearAggregate = aggregate(olympicsData, "Year", "Country")
+        console.log(yearAggregate)
+        let reverseMappings = {};
+        for(let i = 0; i < olympicsData.length; i++) {
+            if(olympicsData[i]['mapId']) {
+                reverseMappings[olympicsData[i]['mapId']] = olympicsData[i]['Country']
 
             }
 
-            console.log(countryData)
-            console.log(discrepentList)
-        });
+        }
+         //console.log(reverseMappings)
+        let map = new WorldMap(yearAggregate, reverseMappings)
+
+
+        map.drawMap(mapData, "2012");
+
+    });
 
 
 
-        /*for(let i = 0; i < countries.length; i++) {
-            geocoder.geocode({'address': countries[i]['Country']}, function(results, status) {
-                if (status === 'OK') {
-                    countries[i]['location'] = results
-                    countries[i]['lat'] = results[0].geometry.location.lat()
-                    countries[i]['long'] = results[0].geometry.location.lng()
-                    //console.log(countries[i])
-                } else {
-                   // alert('Geocode was not successful for the following reason: ' + status);
+
+
+
+
+});
+
+function setMapIds(countryData, olympicsData) {
+    d3.csv("data/pop.csv").then(data => {
+        let countryMap = {};
+        let countryNameMap = {}
+        let geoMap = {};
+        for(let i = 0; i < countryData.length; i++) {
+            let discrepent = true;
+            countryNameMap[countryData[i]['Code']] = countryData[i]['Country']
+
+            for(let j = 0; j < data.length; j++) {
+                if((countryData[i]['Country'].toLowerCase() === data[j]['country'].toLowerCase())) {
+                    countryMap[countryData[i]['Code']] = data[j]['geo'];
+                    discrepent = false;
                 }
-            });
+                else if ((countryData[i]['Code'].toLowerCase() === data[j]['geo'].toLowerCase())) {
+                    countryMap[countryData[i]['Code']] = data[j]['geo']
 
+                    discrepent =false;
+                }
+            }
+            if(discrepent) {
+                countryData[i]['mapId'] = 'NA';
+            }
+        }
 
-
-        }*/
-
-
-        d3.csv("data/summer.csv").then(data => {
-            olympicsData = data
-           // console.log(data)
-            let yearAggregate = aggregate(olympicsData, "Year", 'Country')
-            //console.log(yearAggregate)
-            let countryAggregate = aggregate(olympicsData, 'Year', 'Country')
-            //console.log(countryAggregate)
-            let sportAggregate = aggregate(olympicsData, 'Sport', 'Athlete')
-            //console.log(sportAggregate)
-
-        });
-
-
-
-
-
-
-
-
-
-        // Country wise aggregation
-
-
+        for(let i = 0; i < olympicsData.length; i++) {
+            olympicsData[i]['countryName'] = countryNameMap[olympicsData[i]['Country']]
+            olympicsData[i]['mapId'] = countryMap[olympicsData[i]['Country']]
+        }
 
 
     });
 
-    function aggregate(data, param, groupParam) {
-       let aggregatedData = {}
-       for(let i = 0; i < data.length; i++) {
-           let paramValue = data[i][param]
-           let groupParamValue  = groupParam ? data[i][groupParam] : ""
 
 
-           if((!aggregatedData.hasOwnProperty(paramValue))) {
-               aggregatedData[paramValue] = {}
-               if(!groupParam) {
-                   aggregatedData[paramValue]['medals'] = {}
-                   aggregatedData[paramValue]['medals']['gold'] = 0;
-                   aggregatedData[paramValue]['medals']['silver'] = 0;
-                   aggregatedData[paramValue]['medals']['bronze'] = 0;
-               }
-               else {
-                   if(!aggregatedData[paramValue].hasOwnProperty(groupParamValue)) {
-                       aggregatedData[paramValue][groupParamValue] = {}
-                   }
+/*
+function init() {
+    initMap();
+}
 
-                   aggregatedData[paramValue][groupParamValue]['medals'] = {}
-                   aggregatedData[paramValue][groupParamValue]['medals']['gold'] = 0;
-                   aggregatedData[paramValue][groupParamValue]['medals']['silver'] = 0;
-                   aggregatedData[paramValue][groupParamValue]['medals']['bronze'] = 0;
-               }
-           }
-
-           if(groupParamValue && !aggregatedData[paramValue].hasOwnProperty(groupParamValue)) {
-               aggregatedData[paramValue][groupParamValue] = {}
-               aggregatedData[paramValue][groupParamValue]['medals'] = {}
-               aggregatedData[paramValue][groupParamValue]['medals']['gold'] = 0;
-               aggregatedData[paramValue][groupParamValue]['medals']['silver'] = 0;
-               aggregatedData[paramValue][groupParamValue]['medals']['bronze'] = 0;
-           }
+function initMap() {
+    let countryData = loadFile()
+    console.log(olympicsData)
+    let yearAggregate = {}
+    let self = this
 
 
-           if(data[i]['Medal'] === 'Gold') {
-               if(!groupParamValue) {
-                   aggregatedData[paramValue]['medals']['gold']++
-               }
-               else{
-                   aggregatedData[paramValue][groupParamValue]['medals']['gold']++
-               }
-           }
-           else if(data[i]['Medal'] === 'Silver') {
-               if(!groupParamValue) {
-                   aggregatedData[paramValue]['medals']['silver']++
-               }
-               else{
-                   aggregatedData[paramValue][groupParamValue]['medals']['silver']++
-               }
-           }
-           else if(data[i]['Medal'] === 'Bronze') {
-               if(!groupParamValue) {
-                   aggregatedData[paramValue]['medals']['bronze']++
-               }
-               else{
-                   aggregatedData[paramValue][groupParamValue]['medals']['bronze']++
-               }
-           }
-       }
-       return aggregatedData;
-    }
+
+
+
+
+    d3.csv().then(countries => {
+
+        d3.csv().then(data => {
+          //  console.log(data)
+            olympicsData = data
+            countryData = countries
+            yearAggregate = aggregate(olympicsData, "Year", 'Country')
+            d3.json('data/world.json').then(mapData => {
+
+                setMapIds(countryData, olympicsData)
+                //console.log(olympicsData)
+                let reverseMappings = {};
+                for(let i = 0; i < olympicsData.length; i++) {
+                    reverseMappings[olympicsData[i]['mapId']] = olympicsData[i]['Country']
+                }
+             //  console.log(reverseMappings)
+                let map = new Map(yearAggregate, reverseMappings)
+
+
+                map.drawMap(mapData, "2012");
+
+
+            });
+
+
+        });
+
+
+    });*/
+
+
+
 
 
 
 
 
 }
+
+async function loadFile(file) {
+    let data = await d3.csv(file).then(d => {
+        let mapped = d.map(g => {
+            for (let key in g) {
+                let numKey = +key;
+                if (numKey) {
+                    g[key] = +g[key];
+                }
+            }
+            return g;
+        });
+        return mapped;
+    });
+    return data;
+}
+
+async function loadData() {
+    let countryData = await loadFile("data/dictionary.csv");
+    let olympicsData = await loadFile("data/summer.csv");
+    let pop = await loadFile('data/pop.csv');
+
+    //return [pop, gdp, tfr, cmu, life];
+    return {
+        'olympicsData': olympicsData,
+        'countryData': countryData,
+        'pop': pop,
+
+    };
+}
+
+function aggregate(data, param, groupParam) {
+    let aggregatedData = {}
+    for(let i = 0; i < data.length; i++) {
+        let paramValue = data[i][param]
+        let groupParamValue  = groupParam ? data[i][groupParam] : ""
+        if(!groupParamValue) {
+            continue;
+        }
+
+
+        if((!aggregatedData.hasOwnProperty(paramValue))) {
+            aggregatedData[paramValue] = {}
+            if(!groupParam) {
+                aggregatedData[paramValue]['medals'] = {}
+                aggregatedData[paramValue]['medals']['gold'] = 0;
+                aggregatedData[paramValue]['medals']['silver'] = 0;
+                aggregatedData[paramValue]['medals']['bronze'] = 0;
+            }
+            else {
+                if(!aggregatedData[paramValue].hasOwnProperty(groupParamValue)) {
+                    aggregatedData[paramValue][groupParamValue] = {}
+                }
+
+                aggregatedData[paramValue][groupParamValue]['medals'] = {}
+                aggregatedData[paramValue][groupParamValue]['medals']['gold'] = 0;
+                aggregatedData[paramValue][groupParamValue]['medals']['silver'] = 0;
+                aggregatedData[paramValue][groupParamValue]['medals']['bronze'] = 0;
+            }
+        }
+
+        if(groupParamValue && !aggregatedData[paramValue].hasOwnProperty(groupParamValue)) {
+            aggregatedData[paramValue][groupParamValue] = {}
+            aggregatedData[paramValue][groupParamValue]['medals'] = {}
+            aggregatedData[paramValue][groupParamValue]['medals']['gold'] = 0;
+            aggregatedData[paramValue][groupParamValue]['medals']['silver'] = 0;
+            aggregatedData[paramValue][groupParamValue]['medals']['bronze'] = 0;
+        }
+
+
+        if(data[i]['Medal'] === 'Gold') {
+            if(!groupParamValue) {
+                if(!aggregatedData[paramValue]['medals']) {
+                    let x = 0;
+                }
+                aggregatedData[paramValue]['medals']['gold']++
+            }
+            else{
+                if(!aggregatedData[paramValue]) {
+                    let x = 0;
+                }
+                aggregatedData[paramValue][groupParamValue]['medals']['gold']++
+            }
+        }
+        else if(data[i]['Medal'] === 'Silver') {
+            if(!groupParamValue) {
+                aggregatedData[paramValue]['medals']['silver']++
+            }
+            else{
+                aggregatedData[paramValue][groupParamValue]['medals']['silver']++
+            }
+        }
+        else if(data[i]['Medal'] === 'Bronze') {
+            if(!groupParamValue) {
+                aggregatedData[paramValue]['medals']['bronze']++
+            }
+            else{
+                aggregatedData[paramValue][groupParamValue]['medals']['bronze']++
+            }
+        }
+    }
+    return aggregatedData;
+}
+
+
 
